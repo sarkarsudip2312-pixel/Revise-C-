7ec1ecc9db39dcb0979a101c24a38b205d781f63
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-08 11:18:57 +0600

    Add files via upload

---
+++ Revise01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    float x;
+    
+    a=15;
+    x=4.58;
+    
+    cout<<"Your Nummber: "<<a;
+    cout<<"\nYour point: "<<x;
+    
+    return 0;
+}
