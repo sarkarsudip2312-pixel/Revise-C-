ab952fff3eb18aaeb711e9d87c9f18687cc702ad
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-08 11:19:51 +0600

    Add files via upload

---
+++ Revise03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    float x;
+    float y;
+    float sum;
+    
+    cout<<"Enter Your HSC GPA: ";
+    cin>>x;
+    cout<<"\nEnter Your SSC GPA: ";
+    cin>>y;
+    
+    sum=x+y;
+    
+    cout<<"Full GPA: "<<sum;
+    
+    return 0;
+}
