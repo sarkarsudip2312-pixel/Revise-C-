6038e3c4c5fefd91c0bd753c84053fb1ea12e699
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-08 11:19:17 +0600

    Add files via upload

---
+++ Revise02.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int sum;
+    
+    cout<<"Bangla       : ";
+    cin>>a;
+    cout<<"\nEnglish      : ";
+    cin>>b;
+    cout<<"\nMath         : ";
+    cin>>c;
+    cout<<"\nPhysics      : ";
+    cin>>d;
+    cout<<"\nChemistry    : ";
+    cin>>e;
+    cout<<"\nBiology      : ";
+    cin>>f;
+    cout<<"\nHighter Math : ";
+    cin>>g;
+  
+    
+    sum=a+b+c+d+e+f+g;
+    
+    cout<<"\n\nFull Marks: "<<sum;
+    
+    return 0;
+}
