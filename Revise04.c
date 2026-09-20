c19480093391b2595e4b78d61c316de033a6caf6
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-09-20 14:16:29 +0600

    Add files via upload

---
+++ Revise04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    
+    int i;
+    int marks[i];
+    
+    for(i=1; i<=5; i++){
+    scanf("%d", &marks[i]);
+    }
+    
+    for(i=1; i<=5; i++){
+        printf("your Answer:%d\n", marks[i]);
+    }
+    
+    return 0;
+}
